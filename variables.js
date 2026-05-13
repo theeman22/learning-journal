bd49b41b8ba4be6722aeb0cc1fb4e66b0d26fae0
ethangreenberg <ethan@campminder.com>
2026-05-13 17:06:28 -0600

    Add variables.js with let/const examples

---
+++ variables.js
@@ -0,0 +1,21 @@
+const firstName = "Ethan";
+const lastName = "Greenberg";
+const dob = "1989-06-30";
+let dogname = "Stella";
+let age = 36;
+let isStudent = false;
+let playsgolf = true;
+let homestate = 'Colorado'
+let favoritefood = 'Pizza'
+let favoritenumber = 22
+
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Date of Birth: " + dob);
+console.log("Dog Name: " + dogname);
+console.log("Age: " + age);
+console.log("Is Student: " + isStudent);
+console.log("Plays Golf: " + playsgolf);
+console.log("Homestate: " + homestate);
+console.log("Favorite Food: " + favoritefood);
+console.log("Favorite Number: " + favoritenumber);
