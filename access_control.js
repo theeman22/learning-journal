e8384f120f796e93c7077f725b5a476d63ef71d0
ethangreenberg <ethan@campminder.com>
2026-05-14 14:51:59 -0600

    Add access control logic and FizzBuzz implementation in new files

---
+++ access_control.js
@@ -0,0 +1,14 @@
+const role = "test";
+if (role === "admin") {
+    console.log("You have access to all features");
+} else if (role === "editor") {
+    console.log("You have access to edit features");
+} else if (role === "viewer") {
+    console.log("You have access to view features");
+} else {
+    console.log("You have no access");
+}
+
+
+
+
