53d1d94dccc49a25231191f3e26cdd550958dde1
ethangreenberg <ethan@campminder.com>
2026-05-28 10:10:03 -0600

    Add variables.ts with explicit TypeScript type annotations

---
+++ Month 1/Week 3/variables.ts
@@ -0,0 +1,21 @@
+const firstName: string = "Ethan";
+const lastName: string = "Greenberg";
+const dob: string = "1989-06-30";
+let dogname: string = "Stella";
+let age: number = 36;
+let isStudent: boolean = false;
+let playsgolf: boolean = true;
+let homestate: string = 'Colorado'
+let favoritefood: string = 'Pizza'
+let favoritenumber: number = 22
+
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Date of Birth: " + dob);
+console.log("Dog Name: " + dogname);
+console.log("Age: " + age);
+console.log("Is Student: " + isStudent);
+console.log("Plays Golf: " + playsgolf);
+console.log("Homestate: " + homestate);
+console.log("Favorite Food: " + favoritefood);
+console.log("Favorite Number: " + favoritenumber);
